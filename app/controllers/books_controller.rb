864e6d8a2cafb14a1aa05d7e9350151e74a94baa
EC2 Default User <ec2-user@ip-172-31-2-253.ap-northeast-1.compute.internal>
2022-06-27 14:20:56 +0000

    [Update]Bookers2.2 git commit -m[Update]Bookers2.2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,23 @@
+class BooksController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+  
+  def create
+  end
+
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+end
